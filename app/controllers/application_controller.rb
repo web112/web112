16e4d3a379c3f317596532174d453b25ec8de1c7
lizhaogang <web112@126.com>
2011-07-18 17:26:08 +0800

    second try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   private
-  
-  def current_cart
+  dd
+  def ddddcurrent_cart
     Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
     cart = Cart.create
     session[:cart_id] = cart.id
+    ddd
     cart
   end
 end
