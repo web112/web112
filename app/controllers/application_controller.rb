c45622fed3f855636cd629015febc8ff4a6ba487
zhouyy <zhou_good1125@yahoo.com.cn>
2011-07-19 11:38:05 +0800

    'add'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,14 @@ class ApplicationController < ActionController::Base
   
   def current_cart
     Cart.find(session[:cart_id])
+  
   rescue ActiveRecord::RecordNotFound
     cart = Cart.create
     session[:cart_id] = cart.id
     cart
   end
 end
+
+
+
+
